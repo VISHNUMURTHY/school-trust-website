fb0824054e31aa94e0ed88c170423f99a1e08070
R M VISHNU MURTHY <vishnu.vishnu.murthy5@gmail.com>
2020-04-01 06:15:52 +0530

    personal data form done

--- src/app/donate/donate.component.ts
+++ src/app/donate/donate.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { ValidationMessages } from '../validations/validation.messages';
+import { GENDER_TYPES, SALUTATION_TYPES } from '../constants/app.constants';
 
 @Component({
   selector: 'app-donate',
@@ -10,13 +11,27 @@ import { ValidationMessages } from '../validations/validation.messages';
 export class DonateComponent implements OnInit {
 
   step = 0;
+  checked = new FormControl(true);
+  disableAddressPanel = true;
+  disablePaymentPanel = true;
   donorForm: FormGroup;
   errorMessages = ValidationMessages.validationErrorMessages;
+  genderTypes = GENDER_TYPES;
+  salutationTypes = SALUTATION_TYPES;
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.initializeForms();
+
+    this.checked.valueChanges.subscribe((value: boolean) => {
+      if (value === true) {
+        this.donorForm.get('personalDataForm').get('pancard').setValidators([Validators.required, Validators.pattern('^([a-zA-Z]){5}([0-9]){4}([a-zA-Z]){1}?$')]);
+      }else{
+        this.donorForm.get('personalDataForm').get('pancard').clearValidators();
+      }
+      this.donorForm.get('personalDataForm').get('pancard').updateValueAndValidity();
+    });
   }
 
   initializeForms() {
@@ -30,22 +45,56 @@ export class DonateComponent implements OnInit {
       ])],
       mobile: ['', Validators.compose([
         Validators.required,
-        Validators.pattern('^[0-9]{6,}$')
+        Validators.pattern('^[0-9]+$')
+      ])],
+      gender: ['', Validators.compose([
+        Validators.required
+      ])],
+      pancard: ['']
+    });
+
+    let address = this.fb.group({
+      addressDetails: ['', Validators.compose([
+        Validators.required, Validators.pattern('^([A-Za-z0-9#/,-]+\\s)*[A-Za-z0-9#/,-]+$')
+      ])],
+      landMark: ['', Validators.compose([
+        Validators.required, Validators.pattern('^([A-Za-z0-9,]+\\s)*[A-Za-z0-9,]+$')
+      ])],
+      city: ['', Validators.compose([
+        Validators.required, Validators.pattern('^([A-Za-z]+\\s)*[A-Za-z]+$')
+      ])],
+      district: ['', Validators.compose([
+        Validators.required, Validators.pattern('^([A-Za-z]+\\s)*[A-Za-z]+$')
+      ])],
+      state: ['', Validators.compose([
+        Validators.required, Validators.pattern('^([A-Za-z]+\\s)*[A-Za-z]+$')
+      ])],
+      nation: ['', Validators.compose([
+        Validators.required, Validators.pattern('^([A-Za-z]+\\s)*[A-Za-z]+$')
       ])]
     });
 
     let addressData = this.fb.group({
-
+      residenceAddress: address,
+      officeAddress: address
     });
 
     let paymentData = this.fb.group({
-
+      paymentType: ['', Validators.compose([
+        Validators.required, Validators.pattern('^[A-Z]+$')
+      ])],
+      amount: ['', Validators.compose([
+        Validators.required, Validators.pattern('^[0-9]+$')
+      ])]
     });
 
     this.donorForm = this.fb.group({
-      personalDataForm : personalData,
-      addressDataForm : addressData,
-      paymentDataForm : paymentData
+      personalDataForm: personalData,
+      addressDataForm: addressData,
+      paymentDataForm: paymentData,
+      hardCopy: ['', Validators.compose([
+        Validators.required
+      ])]
     });
   }
 
