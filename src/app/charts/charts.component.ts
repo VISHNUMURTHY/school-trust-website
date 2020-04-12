ec95345ac5d9edfaf24bc49dd92d5053b15009a7
R M VISHNU MURTHY <vishnu.vishnu.murthy5@gmail.com>
2020-04-13 01:07:39 +0530

    cards added

--- src/app/charts/charts.component.ts
+++ src/app/charts/charts.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, AfterContentInit } from '@angular/core';
+import * as d3 from 'd3';
 @Component({
   selector: 'app-charts',
   templateUrl: './charts.component.html',
   styleUrls: ['./charts.component.scss']
 })
-export class ChartsComponent implements OnInit {
+export class ChartsComponent implements OnInit, AfterContentInit {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterContentInit() {
+    d3.select('div');
   }
 
 }
